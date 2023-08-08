d35c6a44390f24db05b5136f7d6bc25b8f968a4a
Paulocosta31 <linscosta24@gmail.com>
2023-08-07 21:19:33 -0300

    fiz uma alteraçao no Js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 
-function Gerar(){
-   
-const max = Math.ceil(document.querySelector(".max-input").value )
-const min = Math.floor(document.querySelector(".min-input").value )
+function Gerar() {
 
-const result = Math.floor(Math.random() * (max - min) + min);
 
-document.querySelector(".resultado").value = result
+    const max = Math.ceil(document.querySelector(".max-input").value)
+    const min = Math.floor(document.querySelector(".min-input").value)
+
+    if (max >= min) {
+        alert("Valor Minimo sempre tem que ser MENOR que o valor Maximo")
+    } else {
+
+        const result = Math.floor(Math.random() * (max - min) + min);
+
+        document.querySelector(".resultado").value = result
+
+    }
+
+
+
+
 }
+
+
+
+
+
 
